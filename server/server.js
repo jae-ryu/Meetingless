dea6d1f6546087e858441d1c83a2564ee03b260e
Jae Ryu <thejaeryu@gmail.com>
2021-07-27 23:50:59 -0400

    database all setup bitch

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 const PORT = 3000;
 const app = express();
 
-const mongoURI = 'mongodb+srv://jaeryu:asdzxc90@cluster0.hgz8q.mongodb.net/meetinglessDatabase?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://jaeryu:asdzxc90@cluster0.hgz8q.mongodb.net/meetingless?retryWrites=true&w=majority';
+// const mongoURI = 'mongodb://localhost/meetingless';
 
 // const mongoURI =
 //   process.env.NODE_ENV === 'test'
@@ -26,6 +27,41 @@ mongoose.connect(mongoURI, {
   console.log('MongoDB Connected…')
 })
 .catch(err => console.log(err))
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!!!!');
+})
+
+// Schema
+const Schema = mongoose.Schema;
+const TopicSchema = new Schema({
+  topic: String,
+  body: String,
+  message: {
+    name : String,
+    comment : String,
+  }
+});
+
+// Model
+const TopicPost = mongoose.model('TopicPost', TopicSchema);
+
+// test data to save
+const data = {
+  topic: 'First test',
+  body: 'Test test',
+};
+
+// .save();
+const newTopicPost = new TopicPost(data);
+newTopicPost.save((err) => {
+  if (err) {
+    console.log('Oops error');
+  } else {
+    console.log('Data has been saved!!');
+  }
+});
+
 // statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve index.html on the route '/'
@@ -43,7 +79,7 @@ app.use((err, req, res, next) => {
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
-});
+}) ;
 
 
 app.listen(PORT, () => {
